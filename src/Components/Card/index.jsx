0d6ae776c28c86aa9fbd7df2fa061cc98deb9abf
Your Name <you@example.com>
2024-09-17 14:01:39 -0500

    consumiendo la api

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,10 +1,10 @@
-const Card = () => {
+const Card = (data) => {
 
     return (
-        <div className="bg-white cursor-pointer w-60 h-80 rounded-lg">
+        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute m-2 bottom-0 left-0 bg-white/60 rounded-2xl text-black text-sm px-3 py-0.5'>Electronics</span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/610945/pexels-photo-610945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=' healphones' />
+                <span className='absolute m-2 bottom-0 left-0 bg-white/60 rounded-2xl text-black text-sm px-3 py-0.5'>{data.data.category}</span>
+                <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt= {data.data.title} />
                 <div className='absolute top-0 right-0 m-3 p-1 flex justify-center items-center bg-white w-8 h-8 rounded-full'>
                     <span className=' absolute bottom-0 font-medium text-3xl'>
                         +
@@ -12,8 +12,8 @@ const Card = () => {
                 </div>
             </figure>
             <p className=' flex justify-between'>
-                <span className=' text-lg font-light'>Headphone</span>
-                <span className='font-semibold text-xl'>$300</span>
+                <span className=' text-lg font-light'>{data.data.title}</span>
+                <span className='font-semibold text-xl'>${data.data.price}</span>
             </p>
         </div>
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    //fetch('https://api.escuelajs.co/api/v1/products')
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+
+  }, [])
 
   return (
     <Layout>
-      <Card />
+      <div className='grid gap-3 grid-cols-4 w-full max-w-screen-xl'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
