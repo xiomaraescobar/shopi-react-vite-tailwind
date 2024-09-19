cf2301ded0e1bac65f4d132a5e50a8ffe89434e5
Your Name <you@example.com>
2024-09-18 22:40:22 -0500

    eliminando productos del carrito

--- src/Components/MenuOrder/index.jsx
+++ src/Components/MenuOrder/index.jsx
@@ -7,23 +7,33 @@ import OrderCard from '../OrderCard';
 const MenuOrder = () => {
     const context = useContext(ShoppingCartContext)
 
+    //removiendo productos del carrito
+    const handleDelete = (id) => {
+      const filteredProducts = context.cartProducts.filter(product => product.id != id)
+      context.setCartProducts(filteredProducts)
+    }
+
     return (
         <aside className={`product-detail flex-col fixed right-0 border border-black rounded-lg bg-white ${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'}`}>
-        <div className="flex justify-between items-center p-6">
-            <h2 className='font-medium text-2xl'>My Order</h2>
-            <div>
-                <XCircleIcon onClick = { () => context.closeMenuOrder()}
-                    className='h-7 w-7 text-black cursor-pointer'></XCircleIcon>
+            <div className="flex justify-between items-center p-6">
+                <h2 className='font-medium text-2xl'>My Order</h2>
+                <div>
+                    <XCircleIcon onClick={() => context.closeMenuOrder()}
+                        className='h-7 w-7 text-black cursor-pointer'></XCircleIcon>
+                </div>
             </div>
-        </div>
-        <div className='px-6 overflow-i-scroll'>{context.cartProducts.map(product => (
-            <OrderCard 
-            key={product.id}
-            title={product.title}
-            imageUrl={product.image}
-            price={product.price} />
-        ))}</div>
-    </aside >
+            <div className='px-6 overflow-y-scroll'>{context.cartProducts.map(product => (
+                <OrderCard
+                    key={product.id}
+                    id={product.id}
+                    title={product.title}
+                    imageUrl={product.image}
+                    price={product.price} 
+                    handleDelete={handleDelete}
+                    />
+            ))}
+            </div>
+        </aside >
     )
 }
 export default MenuOrder;
